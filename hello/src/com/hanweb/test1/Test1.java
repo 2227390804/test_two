010f11c6ec9a3207a93a8775705194a05f0fb71e
mabenr <mbr322@hanweb.com>
2019-04-29 16:13:20 +0800

    第一次提交

--- hello/src/com/hanweb/test1/Test1.java
+++ hello/src/com/hanweb/test1/Test1.java
@@ -3,5 +3,8 @@ package com.hanweb.test1;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
